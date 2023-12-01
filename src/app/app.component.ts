03d88c28eb79c25df424228051259aea64839fdd
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2023-12-01 17:16:00 +0300

    AngularLoops

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: "./app.componnet.html",
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'loops';
+  meyve:string="";
+  meyveler:string[]=["elma,armut,portakal,greyfut"];
+  add(){
+    this.meyveler.push(this.meyve);
+  this.meyve="";
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
